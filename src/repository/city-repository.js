c92abec7966b3838b7c56555b13b1b7241a3bf94
Ambarish <ambarish.dutta1010@gmail.com>
2023-01-17 17:00:57 +0530

    Added routers to completed create city api

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,9 +1,11 @@
-const { City } = require('../models/city');
+const { City } = require('../models/index');
 
 class CityRepository {
     async createCity({ name }) {
         try {
-            const city = await City.create({ name });
+            const city = await City.create({ 
+                name 
+            });
             return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
